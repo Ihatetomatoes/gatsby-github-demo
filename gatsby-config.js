2e2f40271a8deb6b0a090247aab6867399942b39
Petr Tichy <petr.tichy@carsales.com.au>
2017-11-23 11:14:31 +1100

    copy update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: `/gatsby-github-demo`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [`gatsby-plugin-react-helmet`]
+};
